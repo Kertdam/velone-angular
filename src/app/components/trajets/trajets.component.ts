b1b518714fb6b82b1128eb13a4d85e8c81a78d44
Benjamin Andrieux <benjamin.andrieux.info@gmail.com>
2019-08-26 00:59:35 +0200

    Add TrajetsComponents

---
+++ src/app/components/trajets/trajets.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Trajet } from 'src/app/models/trajet/trajet.model';
+import { TrajetService } from 'src/app/_services/trajet/trajet.service';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import * as L from 'leaflet';
+import 'leaflet-routing-machine';
+
+
+@Component({
+  selector: 'app-trajets',
+  templateUrl: './trajets.component.html',
+  styleUrls: ['./trajets.component.scss']
+})
+export class TrajetsComponent implements OnInit {
+
+  id: number;
+  trajets: Trajet[];
+
+
+  constructor(private trajetService: TrajetService, private route: ActivatedRoute) { }
+
+
+
+
+  ngOnInit() {
+
+    this.trajetService.getTrajets().subscribe(res => {
+      this.trajets = res;
+      console.log(res);
+    }, err => {
+      console.log(err);
+    });
+  }
+}
