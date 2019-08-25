7b236bf2eb8785047fd170139b28d6281985f9f9
Benjamin Andrieux <benjamin.andrieux.info@gmail.com>
2019-08-26 01:21:18 +0200

    Remove all control on leaflet map

--- src/app/components/trajet/trajet.component.ts
+++ src/app/components/trajet/trajet.component.ts
@@ -16,75 +16,78 @@ import 'leaflet-routing-machine';
 export class TrajetComponent implements OnInit {
 
   id: number;
-  trajet: Trajet=new Trajet();
-  
-constructor(private trajetService: TrajetService, private route: ActivatedRoute) { }
+  trajet: Trajet = new Trajet();
 
-ngOnInit() {
-  let carte = L.map('carte',{zoomControl:false }).setView([50.6311634, 3.0599573], 1);
- 
-  let wp = new Array<L.LatLng>()
+  constructor(private trajetService: TrajetService, private route: ActivatedRoute) { }
 
-  this.id = Number(this.route.snapshot.paramMap.get('id'));
-  this.trajetService.getTrajetById(this.id).subscribe(res => {
-    this.trajet = res;
-    // En attendant d'avoir le back pour Etape
-    this.trajet.etapes=[{"id":1,"nom":"Etape1","lattitude":49.8941708,"longitude":2.2956951,"elevation":40.00},
-                        {"id":2,"nom":"Etape2","lattitude":49.9862177,"longitude":2.4444840,"elevation":40.00},
-                        {"id":3,"nom":"Etape3","lattitude":50.0053294,"longitude":2.4786409,"elevation":40.00},
-                        {"id":3,"nom":"Etape3","lattitude":50.0728994,"longitude":2.5306202,"elevation":40.00}];
-    console.log(this.trajet);
-    L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-      attribution: 'carte'
-    }).addTo(carte);
-    // Transform Etape to Waypoint
-    for (let etape of this.trajet.etapes) {
-       wp.push(L.latLng(etape.lattitude, etape.longitude));
-    }
-   
-    //carte.fitBounds(this.detectBounds(this.trajet.etapes));
+  ngOnInit() {
+    let carte = L.map('carte', {zoomControl: false,
+                                keyboard: false,
+                                dragging: false,
+                                boxZoom: false,
+                                doubleClickZoom: false,
+                                scrollWheelZoom: false,
+                                tap: false,
+                                touchZoom: false,
+                               }).setView([50.6311634, 3.0599573], 1);
 
-    // Add waypoints to leaflet map
-    L.Routing.control({
-      waypoints:(wp),
-      routeWhileDragging: false,
-      draggableWaypoints : false,
-      show: false
-    }).addTo(carte);
+    let wp = new Array<L.LatLng>()
 
-    carte.dragging.disable();
-    carte.touchZoom.disable();
-    carte.doubleClickZoom.disable();
-    carte.scrollWheelZoom.disable();
-    carte.boxZoom.disable();
-    carte.keyboard.disable();
+    this.id = Number(this.route.snapshot.paramMap.get('id'));
+    this.trajetService.getTrajetById(this.id).subscribe(res => {
+      this.trajet = res;
+      // En attendant d'avoir le back pour Etape
+      this.trajet.etapes = [{ "id": 1, "nom": "Etape1", "lattitude": 49.8941708, "longitude": 2.2956951, "elevation": 40.00 },
+      { "id": 2, "nom": "Etape2", "lattitude": 49.9862177, "longitude": 2.4444840, "elevation": 40.00 },
+      { "id": 3, "nom": "Etape3", "lattitude": 50.0053294, "longitude": 2.4786409, "elevation": 40.00 },
+      { "id": 3, "nom": "Etape3", "lattitude": 50.0728994, "longitude": 2.5306202, "elevation": 40.00 }];
+      console.log(this.trajet);
+      L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+        attribution: 'carte'
+      }).addTo(carte);
+      // Transform Etape to Waypoint
+      for (let etape of this.trajet.etapes) {
+        wp.push(L.latLng(etape.lattitude, etape.longitude));
+      }
 
-  }, err => {
-    console.log(err);
-  });
-}
-  
-detectBounds(etapes:Etape[]){
-  let maxLat: number;
-  let maxLng: number;
-  let minLat: number;
-  let minLng : number ;
-  
-  for (let etape of this.trajet.etapes) {
-    if(etape.lattitude > maxLat || maxLat==null){
-      maxLat=etape.lattitude; 
-    }
-    if(etape.lattitude < minLat || minLat==null){
-      minLat=etape.lattitude; 
-    }
-    if(etape.longitude > maxLng || maxLng==null){
-      maxLng=etape.longitude; 
-    }
-    if(etape.longitude < minLng || minLng==null){
-      minLng=etape.longitude; 
-    }
+      carte.fitBounds(this.detectBounds(this.trajet.etapes));
+
+      // Add waypoints to leaflet map
+      L.Routing.control({
+        waypoints: (wp),
+        routeWhileDragging: false,
+        autoRoute: true,
+        draggableWaypoints: false,
+        show:false,
+        addWaypoints:false
+      }).addTo(carte);
+
+    }, err => {
+      console.log(err);
+    });
   }
-  return [[maxLat,maxLng],[minLat,minLng]] ;
+
+  detectBounds(etapes: Etape[]) {
+    let maxLat: number;
+    let maxLng: number;
+    let minLat: number;
+    let minLng: number;
+
+    for (let etape of this.trajet.etapes) {
+      if (etape.lattitude > maxLat || maxLat == null) {
+        maxLat = etape.lattitude;
+      }
+      if (etape.lattitude < minLat || minLat == null) {
+        minLat = etape.lattitude;
+      }
+      if (etape.longitude > maxLng || maxLng == null) {
+        maxLng = etape.longitude;
+      }
+      if (etape.longitude < minLng || minLng == null) {
+        minLng = etape.longitude;
+      }
+    }
+    return [[maxLat, maxLng], [minLat, minLng]];
 
   }
 }
