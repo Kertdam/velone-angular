574d1604ac014d0431ce0e13fac5e126cf41bf4c
Andrieux Benjamin <benjamin.andrieux.info@gmail.com>
2019-08-26 10:46:45 +0200

    Fix Marker Leaflet

--- src/app/components/trajet/trajet.component.ts
+++ src/app/components/trajet/trajet.component.ts
@@ -8,6 +8,12 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import * as L from 'leaflet';
 import 'leaflet-routing-machine';
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+  iconUrl: require('leaflet/dist/images/marker-icon.png'),
+  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 @Component({
   selector: 'app-trajet',
   templateUrl: './trajet.component.html',
