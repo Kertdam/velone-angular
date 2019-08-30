5a51cb03e79c16a2289a17b0a930b71ced7a233d
Andrieux Benjamin <benjamin.andrieux.info@gmail.com>
2019-08-30 11:44:08 +0200

    Update
    Get elevation of point

--- src/app/components/test-geocoding/test-geocoding.component.ts
+++ src/app/components/test-geocoding/test-geocoding.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input, OnChanges } from '@angular/core';
 import { GeocodingService } from 'src/app/_services/geocoding/geocoding.service';
+import { ElevationService } from 'src/app/_services/elevation/elevation.service';
 
 @Component({
   selector: 'app-test-geocoding',
@@ -11,7 +12,7 @@ export class TestGeocodingComponent implements OnInit {
   datas: any[] ;
   results: string[];
 
-  constructor(private geocodingService: GeocodingService) { }
+  constructor(private geocodingService: GeocodingService, private elevationService: ElevationService) { }
 
   ngOnInit() {
     this.results=[];
@@ -21,8 +22,13 @@ export class TestGeocodingComponent implements OnInit {
   search(event) {
       this.geocodingService.getGeocode(event.query).subscribe(data => {
         console.log(data);
+        data.features.forEach(feature => {
+            console.log(feature.geometry.coordinates[0],feature.geometry.coordinates[1]);
+           feature.geometry.coordinates[2]= this.elevationService.getElevation(feature.geometry.coordinates[1],feature.geometry.coordinates[0]).subscribe(elev => {
+            feature.geometry.coordinates[2]=elev.data[0];
+            });
+        });
         this.datas = data.features ;
-        
         /*const tmp:string[]=[];
           this.datas = data;
           
@@ -36,6 +42,6 @@ export class TestGeocodingComponent implements OnInit {
 
   handleDropdown(event) {
     event.query = this.results ;
-}
+  }
 
 }
