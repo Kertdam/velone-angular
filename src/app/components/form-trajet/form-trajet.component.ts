80e2a5a9901705cf37d4fd363ce41787d9e1cf59
Andrieux Benjamin <benjamin.andrieux.info@gmail.com>
2019-09-02 17:30:43 +0200

    Update

--- src/app/components/form-trajet/form-trajet.component.ts
+++ src/app/components/form-trajet/form-trajet.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators,FormArray } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
 import { Trajet } from 'src/app/models/trajet/trajet.model';
 import { TrajetService } from 'src/app/_services/trajet/trajet.service';
 import { Utilisateur } from 'src/app/models/utilisateur/utilisateur.model';
 import { DatePipe } from '@angular/common';
-import { Etape } from 'src/app/models/etape/etape.model';
+import { GeocodingService } from 'src/app/_services/geocoding/geocoding.service';
+import { ElevationService } from 'src/app/_services/elevation/elevation.service';
 
 @Component({
   selector: 'app-form-trajet',
@@ -12,52 +13,64 @@ import { Etape } from 'src/app/models/etape/etape.model';
   styleUrls: ['./form-trajet.component.scss']
 })
 export class FormTrajetComponent implements OnInit {
-  datasNom:any[] =[] ;
-  etapes:Etape[] =[];
-  etapesForm:FormArray;
-  trajetForm = this.fb.group({
-    nomCtrl : [''],
-    descCtrl: [''],
-    dateCtrl: ['']
-  });
+  results: string[];
+  datasNom = [];
+  datas : any ;
+  trajetForm: FormGroup;
+  etapes: FormArray;
 
-  constructor(private fb: FormBuilder, private trajetService: TrajetService, private datePipe: DatePipe) {}
+  constructor(private formBuilder: FormBuilder, 
+              private trajetService: TrajetService, 
+              private datePipe: DatePipe, 
+              private geocodingService: GeocodingService,
+              private elevationService: ElevationService) {
+
+  }
 
   ngOnInit() {
-  }
-
-  handleSubmit() {
-    console.log(this.trajetForm.value);
-    let trajet: Trajet = new Trajet();
-    trajet.nom = this.trajetForm.value.nomCtrl;
-    trajet.description = this.trajetForm.value.descCtrl;
-    trajet.dateDepart = this.trajetForm.value.dateCtrl;
-    trajet.dateDepart = this.transformDate(trajet.dateDepart);
-    console.log(trajet.dateDepart);
-    trajet.utilisateur = new Utilisateur();
-    trajet.utilisateur.id = 1;
-    this.trajetService.addTrajet(trajet).subscribe( res => {
-      console.log(res);
-    }, err => {
-      console.log(err);
+    this.trajetForm = this.formBuilder.group({
+      nom: '',
+      description: '',
+      dateDepart: '',
+      etapes: this.formBuilder.array([this.createEtape()])
     });
   }
 
-  transformDate(date) {
-    return this.datePipe.transform(date, 'yyyy-MM-dd HH:mm:ss ');
-  }
-
-  createEtapeGroup(etape:Etape): FormGroup {
-    return this.fb.group({
-        nom: [etape.nom],
-        lattitude: [etape.lattitude],
-        longitude: [etape.longitude],
-        elevation: [etape.elevation],
+  createEtape(): FormGroup {
+    return this.formBuilder.group({
+      nom: '',
+      lattitude: '',
+      longitude: '',
+      elevation: ''
     });
   }
 
-  addEtapeGroup(){
-    const control = <FormArray>this.trajetForm.controls['etapes'];
-    control.push(this.createEtapeGroup(new Etape()));
+  addEtape(): void {
+    this.etapes = this.trajetForm.get('etapes') as FormArray;
+    this.etapes.push(this.createEtape());
   }
+
+  searchData(event,i){
+    let res = this.datas.find(feature => feature.properties.label==event);
+    this.trajetForm.get('etapes').get(`${i}`).get('lattitude').setValue(res.geometry.coordinates[1]);
+    this.trajetForm.get('etapes').get(`${i}`).get('longitude').setValue(res.geometry.coordinates[0]);
+  }
+
+  search(event) {
+    this.geocodingService.getGeocode(event.query).subscribe(data => {
+      this.datas = data.features ;
+      this.datasNom = [];
+      for (const d of data.features) {
+        this.datasNom.push(
+          d.properties.label
+        );
+        console.log(d);
+      }
+    });
+  }
+
+  handleDropdown(event) {
+    event.query = this.results ;
+  }
+
 }
